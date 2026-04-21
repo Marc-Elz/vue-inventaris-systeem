d926e1895a41894a45a3b283cd6d56249ddcb2a9
Marc-E <marcelzinga@live.nl>
2026-04-21 15:37:56 +0200

    Added routes and store with some basic functions and interface

--- src/domains/inventory/routes.ts
+++ src/domains/inventory/routes.ts
@@ -1,7 +1,9 @@
 // import from pages
-
-
+import Overview from "../inventory/pages/Overview.vue";
+import Create from "./pages/Create.vue";
 
 export const routes = [
-    {path: '', component: },
+  { path: "/inventory/overview", component: Overview },
+  { path: "", component: Overview },
+  { path: "/inventory/create", component: Create },
 ];
--- src/domains/inventory/store.ts
+++ src/domains/inventory/store.ts
@@ -1,1 +1,25 @@
 import { ref, computed } from "vue";
+
+interface inventoryItem {
+  id: number;
+  name: string;
+  actualAmount: number;
+  minimumAmount: number;
+}
+
+const items = ref<inventoryItem[]>([
+  { id: 214, name: "DarkSouls1", actualAmount: 20, minimumAmount: 50 },
+  { id: 222, name: "DarkSouls2", actualAmount: 50, minimumAmount: 10 },
+  { id: 213, name: "DarkSouls3", actualAmount: 23, minimumAmount: 20 },
+  { id: 244, name: "NightReign", actualAmount: 20, minimumAmount: 10 },
+  { id: 645, name: "Hollow Knight", actualAmount: 15, minimumAmount: 5 },
+  { id: 236, name: "Elden Ring", actualAmount: 0, minimumAmount: 1 },
+  { id: 227, name: "Silksong", actualAmount: 35, minimumAmount: 20 },
+]);
+
+export const getAllItems = computed(() => items.value);
+export const getItemById = (id: number) =>
+  computed(() => items.value.find((item) => item.id == id));
+export const addItem = (item: inventoryItem) => items.value.push(item);
+
+export type { inventoryItem as inventoryItemType };
